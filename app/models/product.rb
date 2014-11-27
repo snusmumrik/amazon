058f412ef6b23f9d2411171ecdfff33d08f4be22
Hiroyuki Kondo <hiroyuki@chishaku.com>
2014-11-27 13:21:08 +0900

    add before_update for products

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,13 @@ class Product < ActiveRecord::Base
   validates :asin, presence: true
   validates :asin, uniqueness: true
   acts_as_paranoid
+
+  before_update :calculate_cost
+
+  def calculate_cost
+    @exchange_rate = open("public/exchange_rate.txt", "r").read.to_i
+    @shipping_cost = 1080
+    @exchange_rate = open("public/exchange_rate.txt", "r").read.to_i
+    self.profit = self.price * @exchange_rate - @shipping_cost - self.cost
+  end
 end
